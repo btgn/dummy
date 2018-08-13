29d4333f4d5060c9991c76ddea793aebb74792e3
btgn <tbakula@calstatela.edu>
2018-08-13 15:25:22 -0500

    minor changes

--- Loops.java
+++ Loops.java
@@ -2,10 +2,7 @@ package ramesh_ana;
 
 public class Loops {
     public static void main(String[] args) {
-        /*for (int i = 9; i >=0 ; i--) {
-            System.out.println(i);
-        }
-*/
+ 
         int x = 9;
         while (x>=0){
             System.out.println( "Hello World: " + x);
