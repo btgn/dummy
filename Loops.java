e96f93d002ddb9bbb757fbac41a7f584e43e1cab
btgn <tbakula@calstatela.edu>
2018-08-13 15:12:26 -0500

    Initial Commit

---
+++ Loops.java
@@ -0,0 +1,15 @@
+package ramesh_ana;
+
+public class Loops {
+    public static void main(String[] args) {
+        /*for (int i = 9; i >=0 ; i--) {
+            System.out.println(i);
+        }
+*/
+        int x = 9;
+        while (x>=0){
+            System.out.println( "Hello World: " + x);
+            x--;
+        }
+    }
+}
